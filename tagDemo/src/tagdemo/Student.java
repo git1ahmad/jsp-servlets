c33dcdd4587189fda398acb9d5a92a7420c5cea7
ahmad1git <ahmadninf@gmail.com>
2017-01-28 16:59:23 +0600

    student class for data as mvc

---
+++ tagDemo/src/tagdemo/Student.java
@@ -0,0 +1,29 @@
+package com.luv2code.jsp.tagdemo;
+
+public class Student {
+	private String firstName;
+	private String lastname;
+	private boolean goldCustomer;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public boolean isGoldCustomer() {
+		return goldCustomer;
+	}
+	public void setGoldCustomer(boolean goldCustomer) {
+		this.goldCustomer = goldCustomer;
+	}
+	
+	
+	
+
+}
